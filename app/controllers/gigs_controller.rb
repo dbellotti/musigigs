e254654b25629b535a406905eb8a390e80f34a27
Dave Bellotti <dbellotti@gmail.com>
2009-07-13 18:09:23 -0700

    added gig create, list, and date_select for search

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -2,6 +2,7 @@ class GigsController < ApplicationController
 
   def index
     @gigs = Gig.all
+    @user = User.find(session[:user_id])
     
     respond_to do |format|
       format.html # index.html.erb
@@ -10,7 +11,27 @@ class GigsController < ApplicationController
   end
 
   def search
-    'apply for a new gig'
+    @user = User.find(session[:user_id])
+    @gigs = Gig.all
+  end
+  
+
+  def new
+    @gig = Gig.new
+    @gig.band_title = 'dp fresh'
+    @gig.time = Time.now
+
+    respond_to do |format|
+      if @gig.save
+        flash[:notice] = "#{@gig.band_title} successfully created gig at #{@gig.time}."
+        format.html { redirect_to(:action => 'search') }
+        format.xml  { render :xml => @gig, :status => :created }
+      else
+        flash[:notice] = "unable to save, try again?"
+        format.html { render :action => "search" }
+        format.xml  { render :xml => @gig.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
 end
